a889be2b2b034dd035cfd4b99624ee454df456a5
tinamanga <christinamanga28@gmail.com>
2025-04-04 01:28:13 +0300

    learning about for...in and for ..of

---
+++ index.js
@@ -0,0 +1,12 @@
+// //Key Differences Between for...of and for...in
+// for...of is used to iterate over values in an iterable object (like arrays or strings), while for...in is used to iterate over the keys (properties) of an object.
+
+// for...in should not be used to iterate over arrays, as it doesn't guarantee the order of iteration and is generally slower.
+
+// for...of is cleaner and more semantically correct when you need to deal with elements of an array or string, while for...in is ideal for enumerating object properties.
+
+// When to Use Each Loop
+// Use for...of when iterating over arrays, strings, or other iterable objects.
+
+// Use for...in when you need to enumerate the properties of an object.
+
